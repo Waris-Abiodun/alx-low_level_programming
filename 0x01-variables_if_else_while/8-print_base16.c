6f6ca9d37b475e3bf76380b0a217391705036e77
Waris-Abiodun <106810847+Waris-Abiodun@users.noreply.github.com>
2022-07-02 01:42:35 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,31 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- *main - prints all the numbers of base 16 in lowercase, followed by a new line
- *
- *Return: 0
- **/
+ * main - main block to be executed
+ * Description: printing base 10 digits
+ * Return: 0 Success
+ */
 int main(void)
 {
-        /**
-         *
-         * hexadecimal
-         * 0 1 2 3 4 5 6 7 8 9 a c d e f
-         */
-        char ch;
-        int n;
-
-        for (n = 48; n <= 57; n++)
-        {
-        putchar(n);
-        }
-        for (ch = 'a'; ch <= 'f'; ch++)
-        {
-        putchar(ch);
-        }
-        putchar(10);
-
-        return (0);
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
 }
