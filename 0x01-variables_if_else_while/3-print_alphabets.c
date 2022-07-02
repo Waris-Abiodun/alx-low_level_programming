44b02a1186ba13d7e5a3cd789fea467a2fb3cde8
Waris-Abiodun <106810847+Waris-Abiodun@users.noreply.github.com>
2022-07-02 01:35:41 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,28 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- *main - Task 3 prints the alphabet in lowercase, and then in uppercase
- *followed by a new line
- *
- *Return: 0
- **/
+ * main - main block
+ * Description:  prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
-        int ch;
+	char c = 'a';
 
-        for (ch = 97; ch <= 122; ch++)
-        {
-        putchar(ch);
-        }
-        for (ch = 65; ch <= 90; ch++)
-        {
-        putchar(ch);
-        }
-        putchar(10); /* this is the ASCII code for new line*/
-        return (0);
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
