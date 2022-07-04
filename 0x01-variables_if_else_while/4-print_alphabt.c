b59aac1898797a88f1b55ea6786163e6037b9c8b
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-04 15:36:11 -0700

    waris

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * main -> write a letter from a-z in upper and lower case
- * Description:print the alphabet in lower case
- *
- * Return : Always 0 (Success)
+ * Description: print the alphabet in lower case
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char f;
-f = 'a';
-while (f <= 'z')
-{
-	putchar(f);
-}
-if (f != 'q' && f != 'e')
-{
-	f++;
-}
-putchar('\n');
-return (0);
+	char str;
+
+	for (str = 'a' ; str <= 'z' ; str++)
+	{
+		if (str != 'q' && str != 'e')
+		{
+			putchar(str);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
