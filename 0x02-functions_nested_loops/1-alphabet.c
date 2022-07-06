d819e5b38e1bae029d979bc67c63e3624f175ff7
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-06 06:55:26 -0700

    waris

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a letter from a to z
+ * Descriptin: I am still learning
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char f = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(f);
+		f++;
+	}
+	_putchar('\n');
+	return (0);
+}
