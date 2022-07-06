bcf7478e78d1812887806c1d91560c1dc35de345
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-06 14:12:10 -0700

    waris

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98- prints to 98
+ * @a: number start fro 0
+ * Return: 0 or 1 always
+ */
+void print_to_98(int a)
+{
+	while (a >= 98)
+	{
+		printf("%i, ", a);
+		a--;
+	}
+	while (a <= 98)
+	{
+		printf("%i, ", a);
+		a++;
+	}
+	putchar('\n');
+}
