3b601b7b61eee454b9f6b58e6859b7238d671e1d
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-06 08:52:41 -0700

    Waris

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - another fun time with c programming 
+ * Description: a fun test on the return valu
+ * Return: + - 0
+ */
+ int print_sign(int n)
+{
+	int sign;
+
+	if (n == 0)
+	{
+		sign = 0;
+		_putchar('0');
+	}
+	else if (n > 0)
+	{
+		sign = 1
+		_putchar('+');
+	}
+	else (n < 0)
+	{
+		sign = -1;
+		_putchar('-');
+	}
+	return (sign);
+}
