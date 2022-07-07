338405fc5cc517162a5604041628277dc2e5790b
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-07 04:46:23 -0700

    waris

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mian - ffibonnacii from 0 t0 50
+ * Description: coprintgs the fibonnacci
+ * retun: 0
+ */
+int main(void)
+{
+	int i = 0;
+	long int a = 0 , b = 1 ,fib;
+
+	while (i < 50)
+	{
+		fib= a + b;
+		a = b;
+		b = fib;
+		printff("%lu", fib);
+
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i+=;
+	}
+	putchar('\n');
+
+	return (0);
+}
