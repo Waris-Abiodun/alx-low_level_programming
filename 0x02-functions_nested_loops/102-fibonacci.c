392aa04e91a8a3a86b81c588f5acdac98691ce67
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-07 04:57:50 -0700

    waris

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /**
- * mian - ffibonnacii from 0 t0 50
+ * main - ffibonnacii from 0 t0 50
  * Description: coprintgs the fibonnacci
- * retun: 0
+ * Return: 0
  */
 int main(void)
 {
 	int i = 0;
-	long int a = 0 , b = 1 ,fib;
+	long int a = 0, b = 1, fib;
 
 	while (i < 50)
 	{
-		fib= a + b;
+		fib = a + b;
 		a = b;
 		b = fib;
 		printf("%lu", fib);
