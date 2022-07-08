bdea4dc2b604e67ed8fc8c0b0d08abb1c90f02e6
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-08 01:39:43 -0700

    waris

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * mul multiply two integers x and y
+ * @b: first int
+ * @a: second int
+ * Return: multiply x and y
+ */
+int mul (inta , int b)
+	return (a *  b);
