a71f511605bcd03e10ff5087b7a19c00666133dc
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-08 02:42:18 -0700

    waris

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - print astraight line
+ * @n: number of times "_" should be printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i >= 0
+	{
+		_putchar('_');
+		i++
+	}
+	_putchar('\n');
+}
