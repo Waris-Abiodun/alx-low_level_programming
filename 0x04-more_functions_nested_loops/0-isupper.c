6622dc9d94d721060e62ac2508f7298cf2076aff
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-07 21:15:14 -0700

    waris

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,14 @@
 
 /**
  * _isupper - i want o print aupper letter
+ * @c: parameter
  * Desctiption: lets test with range of capital letter
  * Return: 1 for capital letter and 0 for other
  */
 
 int _isupper(int c)
 {
-	for (c = 'A', c <= 'Z', c++)
+	if (c = 'A' && c <= 'z')
 	{
 		_putchar(c);
 		return (1);
