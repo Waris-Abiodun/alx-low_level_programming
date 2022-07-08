2d55963bcffd208419b3c4d7522572b0e28080ba
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-08 16:03:36 -0700

    waris

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - i want o print aupper letter
@@ -11,7 +10,7 @@
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'z')
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
