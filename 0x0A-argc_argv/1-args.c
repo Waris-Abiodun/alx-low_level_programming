c971834a7720060000203cb75aadc5eee885b657
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-25 08:11:02 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of  args
+ * @argv: array args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
