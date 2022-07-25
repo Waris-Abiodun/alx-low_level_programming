27fa51a44b9c1ed03bd8f19b2752c47929ab1a37
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-07-25 09:00:58 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 /**
- * main - prints the minimum number of coins to make change for an amount.
- * of money.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int value, c;
 
-	if (argc == 1 || argc > 2)
+	c = 0;
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	value = atoi(argv[1]);
 
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
+	else if (value < 0)
 	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
+		printf("%d\n", 0);
+		return (0);
 	}
-	printf("%d\n", ncoins);
+	else if (value % 25 >= 0)
+	{
+		c += value / 25;
+		value = value % 25;
+	}
+	else if (value % 10 >= 0)
+	{
+		c += value / 10;
+		value = value % 10;
+	}
+	else if (value % 5 >= 0)
+	{
+		c += value / 5;
+		value = value % 5;
+	}
+	else if (value % 2 >= 0)
+	{
+		c += value / 2;
+		value = value % 2;
+	}
+	else if (value % 1 >= 0)
+	{
+		c += value / 1;
+	}
+	printf("%d\n", c);
 	return (0);
+}
