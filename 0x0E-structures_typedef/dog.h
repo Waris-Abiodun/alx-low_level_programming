53f6b42f0296857d47106c6588fab7620215d342
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-01 08:10:03 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/*
+ * struct dog - the data structure for dog
+ * @name: the name o the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
