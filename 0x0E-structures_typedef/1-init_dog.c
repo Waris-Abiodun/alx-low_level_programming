9b0a45ece4f8bac7878bf4a2d6a4cb459a06d9f0
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-01 09:01:58 -0700

    dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <dog.h>
+
+/**
+ * init_dog - calling function
+ * @d: array
+ * @name: the name of the dog
+ * @age: the age of the dog to be input and returned
+ * @owner: owner of the dog to be input and returned as a pointer
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if(d == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		(*d).name = name;
+		d->.age = age;
+		d->owner = owner;
+	}
+
+}
