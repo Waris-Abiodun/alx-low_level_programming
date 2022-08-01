1b822d26a5568d71fe7d0ba00a07652d4f379481
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-01 12:08:03 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,13 @@
  *
  * Description: An header function to beuse with all this code
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 #endif /*DOG_H*/
