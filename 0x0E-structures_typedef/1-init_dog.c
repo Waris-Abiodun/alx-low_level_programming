9c3ee1df386929163222d3d9fa9b661afb77b84a
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-01 09:10:28 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,11 +12,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 	if (d == NULL)
 	{
-		return;
+		return (1);
 	}
 	else
 
-		(*d).name = name;
+		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
