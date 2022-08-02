01db7b46da71a804bd4c781545bf203c90054121
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-02 09:41:39 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef 0-OBJECT_LIKE_MACRO_H
-#define 0-OBJECT_LIKE_MACRO_H
+#ifndef MACRO_H
+#define MACRO_H
 
 #define SIZE 1024
-#endif /*0-object_like_macro.h*/
+#endif /* 0-object_like_macro.h */
