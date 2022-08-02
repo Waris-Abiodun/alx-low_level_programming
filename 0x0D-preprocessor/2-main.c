7b492f8636952d27040c2059c4af6bb696b629f7
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-02 09:58:50 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -  main of the function
+ * Description: a function that print file name
+ * Return: an integer of zero
+ */
+int main(void)
+{
+	printf("%s \n", _FILE_);
+	return (0);
+}
