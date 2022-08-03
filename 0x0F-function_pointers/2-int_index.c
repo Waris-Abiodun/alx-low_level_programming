1c32fb3d05e6956a7ecec45e6e48b97767ec320a
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-03 16:15:58 -0700

    if

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,12 +4,14 @@
  * int_index - a function to check index
  * @array: array to be counted or check the index of a certain integer
  * @size: size of the array
- * @cmp: function pointer to 
+ * @cmp: function pointer to
+ * Return: am integer of -1 if nothing is there
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
 	if (size > 0)
 	{
 		if (array != NULL && cmp != NULL)
