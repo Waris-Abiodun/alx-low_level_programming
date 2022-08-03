566e644a77d549c673bcdd99ec00b3507b13a20f
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-03 15:57:17 -0700

    if

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function to check index
+ * @array: array to be counted or check the index of a certain integer
+ * @size: size of the array
+ * @cmp: function pointer to 
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]))
+					return (i);
+			}
+		}
+	}
+	return (-1);
+}
