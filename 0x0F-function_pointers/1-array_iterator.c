30b7d9244fa1865772217a743bbe6435f6e23923
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-03 15:43:21 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function ppointer that print an array
+ * @array: array to be printed using loop
+ * @size: size of the array, how many times we are going throgh the loop
+ * @action: the function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && size > 0 && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+
+	}
+}
