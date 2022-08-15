b477abd91c2af1bb413c7dde14b8d73409051d1e
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-15 16:57:19 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that pint the data in the node
+ * @h: nodes to be passed
+ * Return: the number ofnodes we printed
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (k != NULL)
+	{
+		k = k->next;
+		node++;
+	}
+	return (node);
+}
