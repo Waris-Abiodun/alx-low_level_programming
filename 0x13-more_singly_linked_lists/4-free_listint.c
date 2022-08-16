5c865b77d2c056a9fe64e4d34806b387858dec1d
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-15 19:23:19 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - to check if memory are manage effectively
+ * @head: node pointer
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *node = head;
+
+
+	while  (node != NULL)
+	{
+		node = node->next;
+		free(node);
+	}
+
+}
