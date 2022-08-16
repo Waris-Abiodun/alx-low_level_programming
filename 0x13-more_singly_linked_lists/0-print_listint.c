84a7e3a3a68d698d8c31cf0d2cc2c7bf5967692d
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-16 14:22:59 -0700

    memory-task-0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,23 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - a function that pint the data in the node
- * @h: nodes to be passed
- * Return: the number ofnodes we printed
- */
+ * print_listint - Print elements of a singly linked list.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *k;
-	size_t node = 0;
+	const listint_t *tp;
+	unsigned int cntr = 0;
 
-	k = malloc(sizeof(listint_t));
-	k = h;
-	while (k != NULL)
+	tp = h;
+	while (tp)
 	{
-		printf("%d\n", k->n);
-		k = k->next;
-		node++;
+		printf("%d\n", tp->n);
+		cntr++;
+		tp = tp->next;
 	}
-	return (node);
+	return (cntr);
+
 }
