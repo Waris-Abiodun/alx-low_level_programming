54b4b7eb4888f830387009297a8416cdae08d880
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-22 17:47:07 -0700

    correction-task-1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * filename - this will pass the letter we want o print
+ * read_textfile - a function that print texts out base on number of lines
+ * @filename : this will pass the letter we want o print
  * @letters :max number of letter we can print
- * Return : The number of character to be printed
+ * Return: The number of character to be printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -12,9 +13,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buff;
 
-	fd = open(filename , O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (fd != 2)
+	if (fd == -1)
 		return (0);
 
 	buff = malloc(sizeof(char) * letters);
