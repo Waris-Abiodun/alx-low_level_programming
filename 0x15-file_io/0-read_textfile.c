c5a152e493cd3e50f4ce3307ec3a8bd733b45e3b
Kazeem Waris Abiodun <kazeemwaris05@gmail.com>
2022-08-22 17:40:23 -0700

    task-1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * filename - this will pass the letter we want o print
+ * @letters :max number of letter we can print
+ * Return : The number of character to be printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t noRead;
+	ssize_t noWrite;
+	int fd;
+	char *buff;
+
+	fd = open(filename , O_RDONLY);
+
+	if (fd != 2)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	noWrite = write(fd, buff, letters);
+	noRead = read(STDOUT_FILENO, buff, noWrite);
+	close(fd);
+
+	free(buff);
+
+	return (noRead);
+}
