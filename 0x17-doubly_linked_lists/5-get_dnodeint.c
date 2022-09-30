5676a69130ab36c8229d3c75170664a332ba721c
Kazeem Waris <Kazeemwaris05@gmail.com>
2022-09-29 17:41:16 -0700

    get an index and reurn the node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,12 +1,18 @@
-#include "main.h"
-
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - a function that find an index of node
+ * @head: the head of the node
+ * @index: the nth index we are loking for
+ * Return: the node
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	int node = 0;
-	while(head != NULL)
+	unsigned int node = 0;
+
+	while (head != NULL)
 	{
 		if (node == index)
-			return (head->n);
+			return (head);
 		head = head->next;
 		node++;
 	}
