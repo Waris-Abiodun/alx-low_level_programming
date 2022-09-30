ddb9d8a1c9545e1d36d9ce33ebedf4d5ddf08c66
Kazeem Waris <Kazeemwaris05@gmail.com>
2022-09-29 17:48:25 -0700

    adding the value of nodes

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - a function that sum the value of the nodes
+ * @head: the nodes to add
+ * Return: the sum of nodes and zero if the list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != null)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
