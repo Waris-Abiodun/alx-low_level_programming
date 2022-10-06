e75e5ebc3efa4620f4e43cd6f13aa804c070753c
Waris-Abiodun <kazeemwaris05@gmail.com>
2022-10-06 11:57:20 +0100

    operation

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,31 +1,50 @@
 /**
- * add,sub,mul,div and mod - are function that will be used to create shared library
- * 
- * @a: the first integer 
- * @b: the second integer 
- * Return : the result 
+ * add - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
  */
 int add(int a, int b)
 {
-    return (a + b);
+      return (a + b);
 }
-
+/**
+ * sub - subtraction
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
 int sub(int a, int b)
 {
-    return (a - b);
+      return (a - b);
 }
-
+/**
+ * mul - subtraction
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
 int mul(int a, int b)
 {
-    return (a * b);
+      return (a * b);
 }
-
+/**
+ * div - division
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
 int div(int a, int b)
 {
-    return(a / b);
+      return (a / b);
 }
-
+/**
+ * mod - modulos
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
 int mod(int a, int b)
 {
-    return ( a % b);
+      return (a % b);
 }
