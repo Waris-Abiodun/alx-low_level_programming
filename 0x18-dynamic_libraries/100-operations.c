55e834d5201bc5a2b1e9e5b5c5baec84166639d2
Waris-Abiodun <kazeemwaris05@gmail.com>
2022-10-06 16:44:54 +0100

    operation

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+/**
+ * add, sub , mul, divide and mod : are funcions to be use in dynamic library
+ * @a: integr
+ * @b: integer
+ * Return : result of the calc
+ */
+
+ int add(int a, int b)
+ {
+     return (a + b);
+ }
+
+  int sub(int a, int b)
+ {
+     return (a - b);
+ }
+
+  int mul(int a, int b)
+ {
+     return (a * b);
+ }
+
+  int div(int a, int b)
+ {
+     return (a / b);
+ }
+
+  int mod(int a, int b)
+ {
+     return (a % b);
+ }
