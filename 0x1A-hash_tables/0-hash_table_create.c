adc0150208c25b969c3e6adc68e1ccf92cf7e453
Waris Abiodun <kazeemwaris05@gmail.com>
2022-10-20 19:06:25 -0700

    creating the size of an array for hash function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,23 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - a function that create hash table
+ * @size: the limit of our array
+ * Return: null if failed else return the node
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *myHash = NULL;
-    myHash->array = malloc(size + 1);
-    if ((myHash->array) == NULL)
-    {
-        return (NULL);
-    }
-    return (myHash);
+	hash_table_t *myHash =  NULL;
+	myHash = malloc(sizeof(hash_table_t));
+	if (myHash == NULL)
+		return (NULL);
+	myHash->size = size;
+	myHash->array = malloc(sizeof(void *) * size);	
+	if ((myHash->array) == NULL)
+	{
+		free (myHash);
+		return (NULL);
+	}
+	return (myHash);
 }
