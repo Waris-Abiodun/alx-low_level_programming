ecdbcfca992155df37648167b32f635db5886a05
Waris-Abiodun <kazeemwaris05@gmail.com>
2022-10-27 02:59:13 +0100

    key index calculation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * key_index - a function that use the hash function to calculate the key
+ * key_index - a function that use the hash function
+ * to calculate the key
  * @key: the string that will be used to calculate the hash
  * @size: this will be use as modulos 
  * Return: the key index of the string 
