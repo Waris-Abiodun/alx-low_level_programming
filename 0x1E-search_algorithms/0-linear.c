38594249832a4c8193d757eec3b4ad5e2a939f80
Waris <kazeemwaris05@gmail.com>
2023-01-27 22:05:53 +0000

    update

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - a function that searches for a value in an
+ * array of intege sing the Linear search algorithm
+ * @array: where to look for the calue
+ * @size: size of that array
+ * @value: the value to search for
+ * Return: 0 on success
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+printf("welcome back to c");
+return (1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+0x1E-search_algorithms implementing some search algorithm like linear, binary , eponential search etc
