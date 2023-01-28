6649bcd13b6a07a8277609434ed71ff973962d2b
Waris <kazeemwaris05@gmail.com>
2023-01-28 00:20:07 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,15 +14,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	unsigned int i = 0;
 
-	while(i < size)
+	while (i < size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if(array[i] == value)
+		if (array[i] == value)
 		{
 			return (i);
 		}
 		i++;
-		
+
 	}
 	return (-1);
 }
